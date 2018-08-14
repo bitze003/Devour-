9293edfc7478f679bf18f79769fa970fe544b492
Kennet Bitzer <bitze003@d.umn.edu>
2018-08-13 21:31:49 -0500

    finished

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheese", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Veggie", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Turkey", false);
