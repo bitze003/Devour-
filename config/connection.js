a1c07690d3b0521db3f2d1385faf9ee23fe8edc8
Kennet Bitzer <bitze003@d.umn.edu>
2018-08-13 22:03:23 -0500

    addedjaws

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 var mysql = require("mysql");
 
 // Set up our connection information
-var connection = mysql.createConnection({
+var connection;
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "Password",
   database: "burgers_db"
 });
-
+};
 // Connect to the database
 connection.connect(function(err) {
   if (err) {
